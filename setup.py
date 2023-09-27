2b2eb2ab75c40a0cd4aa35f9d49e2a0cdb419565
Altenbernd Mirco (XC-DX/EPO2) <mirco.altenbernd@de.bosch.com>
2023-09-27 20:26:16 +0200

    Increase version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 setup(
     name = "camomilla",
     scripts=['camomilla'],
-    version = "0.2",
+    version = "0.3",
     author = "Vittorio Romeo",
     author_email = "vittorio.romeo@outlook.com",
     description = "C++ error postprocessor"
